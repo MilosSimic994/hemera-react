e764cecb3643791994bbca39078dc2b3f62dcdaf
MilosSimic994 <60389147+MilosSimic994@users.noreply.github.com>
2021-12-26 16:44:45 +0100

    styleing navigation

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const MainNavigation = () => {
+  return (
+    <nav className="main-navigation">
+      <ul>
+        <li>Hemera</li>
+        <li>
+          <NavLink to="/" activeClassName="active-link">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/das" activeClassName="active-link">
+            O nama
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/das" activeClassName="active-link">
+            Proizvodi
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" activeClassName="active-link">
+            Kontact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MainNavigation;
