7802411d3c8fa2b9230a68b8948c785a9c5f4d26
Milos Simic <m.simic@rs.emakina.com>
2021-12-29 15:20:23 +0100

    'header-slider__nice-img'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import data from '../data.json'
 
 
 const Header = () => {
-  const {images} = data
   const [counter, setCounter] = useState(0)
-  const [img,setImg] = useState(images[counter])
-
+  const {images} = data
  
+  const [img, setImg] = useState(images[counter])
 
+  useEffect(() => {
+    if(counter > images.length -1) {
+      setCounter(0)
+    }
+  }, [counter])
+
+ useEffect(() => {
+ let slider = setInterval(() => {
+   setCounter(counter + 1)
+  setImg(images[counter])
+  }, 3000);
+  return ()=> clearInterval(slider)
+ }, [counter])
+ 
+   console.log(images);
   console.log(counter);
   return (
     <header className="header">
