dfaa5f12344bed2cde67e47c8d1a0a653c8a5602
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-01-17 10:05:30 +0100

    card section

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -5,17 +5,17 @@ const CardSection = () => {
 		<section className="cards">
 			<div className="cards__wrapper">
 				<div className="card__item">
-					<img src="/assets/images/8309.png" alt="" />
+					<img src="/assets/images/8309.png" alt="Dostava" />
 					<h2>Dostava</h2>
 					<a href="#">otvori</a>
 				</div>
 				<div className="card__item">
-					<img src="/assets/images/2568.png" alt="" />
+					<img src="/assets/images/2568.png" alt="Asortiman" />
 					<h2>Asortiman</h2>
 					<a href="#">otvori</a>
 				</div>
 				<div className="card__item">
-					<img src="/assets/images/6316.png" alt="" />
+					<img src="/assets/images/6316.png" alt="O nama" />
 					<h2>O nama</h2>
 					<a href="#">otvori</a>
 				</div>
